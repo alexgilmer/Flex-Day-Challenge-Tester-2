f80267eb06453dccad24ab760c597e02ba57622c
Alex Gilmer <alex.gilmer@mitt.ca>
2024-05-21 10:29:17 -0500

    Add calculator and Factorial testers

---
+++ Flex Day Challenge Tester 2/Testers/CalculatorTester.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flex_Day_Challenge_Tester_2.Testers
+{
+    public class CalculatorTester : Tester<string, decimal>
+    {
+        public override string TestName => "Calculator Test";
+
+        public override IList<string> GetTests()
+        {
+            return new List<string>()
+            {
+                "4+6",
+                "17+1",
+                "9+0",
+                "8.5+1.1",
+                "1.1+1.1",
+                "2.0+9.9",
+                "999+1",
+
+                "4-22",
+                "12-5",
+                "1.4-0.9",
+                "2-0",
+                "123-59",
+                "14.44-12.57",
+                "7-7.00",
+
+                "1*1",
+                "2*2",
+                "4*9",
+                "100*0",
+                "1.1*1.111",
+                "5.5*0.1",
+                "999*999",
+
+                "999/999",
+                "999/909",
+                "14/2",
+                "5/7",
+                "0/1",
+                "1/0.001",
+                "4/100000"
+            };
+        }
+
+        public override decimal SolutionFunction(string input)
+        {
+            if (input.Contains('+'))
+            {
+                string[] nums = input.Split('+');
+                decimal num1 = decimal.Parse(nums[0]);
+                decimal num2 = decimal.Parse(nums[1]);
+                return num1 + num2;
+            }
+
+            if (input.Contains('-'))
+            {
+                string[] nums = input.Split('-');
+                decimal num1 = decimal.Parse(nums[0]);
+                decimal num2 = decimal.Parse(nums[1]);
+                return num1 - num2;
+            }
+
+            if (input.Contains('*'))
+            {
+                string[] nums = input.Split('*');
+                decimal num1 = decimal.Parse(nums[0]);
+                decimal num2 = decimal.Parse(nums[1]);
+                return num1 * num2;
+            }
+
+            if (input.Contains('/'))
+            {
+                string[] nums = input.Split('/');
+                decimal num1 = decimal.Parse(nums[0]);
+                decimal num2 = decimal.Parse(nums[1]);
+                return num1 / num2;
+            }
+
+            throw new Exception("No operator found");
+        }
+    }
+}
---
+++ Flex Day Challenge Tester 2/Testers/FactorialTester.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Flex_Day_Challenge_Tester_2.Testers
+{
+    public class FactorialTester : Tester<int, int>
+    {
+        public override string TestName => "Factorial Calculator";
+
+        public override IList<int> GetTests()
+        {
+            return new List<int>()
+            {
+                1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12
+            };
+        }
+
+        public override int SolutionFunction(int input)
+        {
+            int result = 1;
+            for (int i = input; i >= 2; i--)
+            {
+                result *= i;
+            }
+            return result;
+        }
+    }
+}
